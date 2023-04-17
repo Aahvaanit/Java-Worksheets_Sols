adaa7116832edd8b8ceb1f5c138772a3dcbdb2fc
Aahvaanit <tripathi.aahvaanit@gmail.com>
2023-04-17 20:10:33 +0530

    edit

--- worksheets/chap3/problem1.java
+++ worksheets/chap3/problem1.java
@@ -1,3 +1,9 @@
 public class problem1 {
-    
+    public static void main(String args[]) {
+        int n;
+        do {
+            n = IBIO.inputInt("Enter a number below 100: ");
+        } while (n >= 100 || n % 2 == 1 || n <= 0);
+        IBIO.output(n);
+    }
 }
