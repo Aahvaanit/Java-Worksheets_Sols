7ab17b0330a316662eea80aeeb87438c6698a451
Aahvaanit <tripathi.aahvaanit@gmail.com>
2023-04-21 19:38:33 +0530

    .

--- worksheets/chap4/problem1.java
+++ worksheets/chap4/problem1.java
@@ -3,28 +3,23 @@ public class problem1 {
         int a = IBIO.inputInt("Enter first number: ");
         int b = IBIO.inputInt("Enter second number: ");
 
-        IBIO.output("Press:  [1] for addition");
-        IBIO.output("        [2] for multiplication");
-        IBIO.output("        [3] for quit");
-
         int n;
         do {
-            n = IBIO.inputInt("> ");
-        } while (n > 3 || n < 0);
+            IBIO.output("Press:  [1] for addition");
+            IBIO.output("        [2] for multiplication");
+            IBIO.output("        [3] for quit");
+            do {
+                n = IBIO.inputInt("> ");
+            } while (n < 0 || n > 3);
 
-        switch (n) {
-            case 1:
-                IBIO.output(a + b);
-                break;
-            case 2:
-                IBIO.output(a * b);
-                break;
-            case 3:
-                IBIO.output("Exited program.");
-                break;
-            default:
-                IBIO.output("Invalid input.");
-                break;
-        }
+            switch (n) {
+                case 1:
+                    IBIO.output(a + b);
+                    break;
+                case 2:
+                    IBIO.output(a * b);
+                    break;
+            }
+        } while (n != 3);
     }
 }
